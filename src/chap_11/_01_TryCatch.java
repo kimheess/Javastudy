f4841c80233c65f83a52be50d1b9f6dded491ffc
heesu <cucucu1232@naver.com>
2023-03-11 16:20:19 +0900

    java03/11

---
+++ src/chap_11/_01_TryCatch.java
@@ -0,0 +1,35 @@
+package chap_11;
+
+public class _01_TryCatch {
+
+	public static void main(String[] args) {
+		// 예외처리
+		// 오류 : 컴파일 오류, 런타임 오류(에러 error, 예외 exception)
+		
+//		int i = "문자열"; 컴파일 오류 
+		
+//		int [] arr = new int[3];  런타임 오류 
+//		arr[5] = 100;
+		
+		try {
+//			System.out.println(3 / 0);
+			
+//			int[] arr = new int[3];
+//			arr[5] = 100;
+			
+//			Object obj = "test";
+//			System.out.println((int)obj);
+		}	
+		catch (Exception e) {
+			System.out.println("이런 문제가 발생했어요 => " + e.getMessage());
+			e.printStackTrace();
+		}
+		
+			System.out.println("프로그램 정상 종료");
+		
+	
+
+}
+
+
+}
---
+++ src/chap_11/_02_Catch.java
@@ -0,0 +1,39 @@
+package chap_11;
+
+public class _02_Catch {
+
+	public static void main(String[] args) {
+		try {
+//			System.out.println(3 / 0);
+			
+			int[] arr = new int[3];
+			arr[5] = 100;
+//			
+//			Object obj = "test";
+//			System.out.println((int)obj);
+			
+//			String s = null;
+//			System.out.println(s.toLowerCase());
+		}	
+		catch(ArithmeticException | ArrayIndexOutOfBoundsException e) {
+			System.out.println("뭔가 실수 하셨네요");
+		}
+		
+//		catch(ArrayIndexOutOfBoundsException e) {
+//			System.out.println("인덱스를 잘못 설정 했어요 ");
+//		}
+		
+		catch(ClassCastException e) {
+			System.out.println("잘못된 형 변환 입니다.");
+		}
+		
+		catch (Exception e) {
+			System.out.println("그외의 모든 문제는 여기서 해결 돼요");
+			e.printStackTrace();
+		}
+		
+			System.out.println("프로그램 정상 종료");
+
+	}
+
+}
